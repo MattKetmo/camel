2ac2e9e378e488aa8eba422e17f39a5f0844cc92
Matthieu Moquet <matthieu@moquet.net>
2012-09-15 15:46:15 +0200

    Update composer description

--- tests/Camel/Tests/CaseTransformerTest.php
+++ tests/Camel/Tests/CaseTransformerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Camel.
+ *
+ * (c) Matthieu Moquet <matthieu@moquet.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Camel\Tests;
 
 use Camel\CaseTransformer;
@@ -9,13 +18,13 @@ class CaseTransformerTest extends \PHPUnit_Framework_TestCase
 {
     public function testTransform()
     {
-        $from = new Format\CamelCase();
-        $to   = new Format\SnakeCase();
+        $camelCase = new Format\CamelCase();
+        $snakeCase = new Format\SnakeCase();
 
-        $transformer = new CaseTransformer($from, $to);
+        $transformer = new CaseTransformer($camelCase, $snakeCase);
         $this->assertEquals('hello_world', $transformer->transform('helloWorld'));
 
-        $transformer = new CaseTransformer($to, $from);
+        $transformer = new CaseTransformer($snakeCase, $camelCase);
         $this->assertEquals('helloWorld', $transformer->transform('hello_world'));
     }
 }
